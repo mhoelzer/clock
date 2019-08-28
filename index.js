ff36322aeee9233ae5ffa4a39d4de7a093cd50be
Michelle Hoelzer <hoelzermm@gmail.com>
2019-08-28 17:03:08 -0400

    did some styling and fixing js; will do more later

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const secondHand = document.querySelector(".second-hand");
-const minHand = document.querySelector(".minute-hand");
+const minuteHand = document.querySelector(".minute-hand");
 const hourHand = document.querySelector(".hour-hand");
+
+const secondsCounter = 0
+const minutesCounter = 0
+const hoursCounter = 0
 
 const numerals = document.querySelector(".numerals")
 // start with second since changes the most often
 function setDate() {
-    const now = new Date();
+	const now = new Date();
+	
     const seconds = now.getSeconds();
-    const secondsDegrees = (seconds / 60) * 360 + 90; // gives percentage then 360 for full
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // this is offset with the 90 degress offset in css
+    const secondsDegrees = (((seconds + secondsCounter) / 60) * 360) + 90; // gives percentage then 360 for full
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // this is offset with the 90 degress offset in css
+	if (seconds === 59) {
+		secondsCounter += 60;
+	}
 
-    const mins = now.getMinutes();
-    const minsDegrees = (mins / 60) * 360 + 90;
-    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+	const minutes = now.getMinutes();
+    const minutesDegrees = (((minutes + minutesCounter) / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-    const hours = now.getMinutes();
-    const hoursDegrees = (hours / 12) * 360 + 90;
+    const hours = now.getHours();
+    const hoursDegrees = (((hours + hoursCounter) / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 	// console.log(seconds)
-	
-	numerals.innerHTML = `${hours}:${mins}:${seconds < 10 ? "0" + seconds : seconds}`
+
+	let secondsDigits = seconds < 10 ? "0" + seconds : seconds
+	let minutesDigits = minutes < 10 ? "0" + minutes : minutes
+	let hoursDigits = hours < 10 ? "0" + hours : hours
+	numerals.innerHTML = `${hoursDigits}:${minutesDigits}:${secondsDigits}`
 }
 setInterval(setDate, 1000); // runs every second
